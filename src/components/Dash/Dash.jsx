75d3789aa21f50778730c1586507e6738b372be3
antoan <a.chavdarov@magazinidar.com>
2024-02-26 17:00:15 +0200

    axios problem

--- src/components/Dash/Dash.jsx
+++ src/components/Dash/Dash.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import DashHeader from "./DashHeader";
-import axios from "axios";
+// import axios from "axios";
 import DashProto from "./DashProto";
 import { Outlet } from "react-router-dom";
 import DashTabs from "./DashTabs";
@@ -12,21 +12,21 @@ import { Draggable } from "react-beautiful-dnd";
 function Dash({ obekt, kasi }) {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchedData = async () => {
-      setLoading(true);
-      try {
-        const { data: response } = await axios.get(
-          "http://192.168.0.145:4002/protokoli"
-        );
-        setData(JSON.parse(response.allProtocols[3].data));
-      } catch (error) {
-        console.log(error.message);
-      }
-      setLoading(false);
-    };
-    fetchedData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchedData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const { data: response } = await axios.get(
+  //         "http://192.168.0.145:4002/protokoli"
+  //       );
+  //       setData(JSON.parse(response.allProtocols[3].data));
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchedData();
+  // }, []);
 
   console.log(data);
   return (
